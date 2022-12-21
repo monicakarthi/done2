fb0d05af55f2b7e11447a55c08fa476b3a23df18
monicakarthi <monicapalaniaero@gmail.com>
2022-12-21 17:27:32 +0530

    to branch

---
+++ Dor1.java
@@ -0,0 +1,29 @@
+package pratice.com;
+import Reliance.com.Bridges;
+
+
+public class Dor1 {
+	void eat() {
+		
+System.out.println("child s eating");
+	}
+	
+	
+
+public void dance() {
+	System.out.println("Child s dancing");
+	}
+public static void main(String[] args) {
+	Dor1 data = new Dor1();
+	data.dance();
+	data.eat();
+	Bridges data1 = new Bridges();
+	data1.Primark_Cafe();
+	
+	
+	
+}
+
+}
+
+
